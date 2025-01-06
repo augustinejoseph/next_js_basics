7dfd0335f8021af15868d2949f85672f74612cb9
Augustine Joseph <augustine@Augustines-MacBook-Pro.local>
2025-01-06 22:38:47 +0530

    Feature/ Common loader added

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+export default function Loading() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "yellowgreen",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      Loading...
+    </div>
+  );
+}
