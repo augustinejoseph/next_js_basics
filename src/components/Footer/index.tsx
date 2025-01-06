a514ec466a5683a7a6bdf88708f0993284ba2b63
Augustine Joseph <augustine@Augustines-MacBook-Pro.local>
2025-01-06 17:33:52 +0530

    Feature/ custom layouts

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    <>
+      <h1
+        style={{
+          backgroundColor: "lightblue",
+          padding: "1rem",
+        }}
+      >
+        Footer
+      </h1>
+    </>
+  );
+};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <>
+      <h1
+        style={{
+          backgroundColor: "lightblue",
+          padding: "1rem",
+        }}
+      >
+        Header
+      </h1>
+    </>
+  );
+};
